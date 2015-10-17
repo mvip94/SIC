87bc4b39adfa4b82a4864b9ece9b04da47dfc510
jorge <jorge@MAMA>
2015-10-17 10:23:54 -0600

    HOla

--- src/lab06swing/Lab06Swing.java
+++ src/lab06swing/Lab06Swing.java
@@ -16,6 +16,7 @@ public class Lab06Swing {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
