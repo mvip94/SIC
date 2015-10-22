05d6ff93c08469df34a3fa97ab6c6c2c4f10f098
mario <mario@Mario-Probook>
2015-10-22 00:39:34 -0600

    Ventanas de costos agregadas

--- src/sistemacontable/LoginController.java
+++ src/sistemacontable/LoginController.java
@@ -1,8 +1,12 @@
-
 package sistemacontable;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,13 +22,18 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
  *
  * @author mario
  */
-public class LoginController implements Initializable{//, ControlledScreen {
+public class LoginController implements Initializable {//, ControlledScreen {
+//atributo de conexion a la base de datos
+
+    private Connection connection;
+//finalizacion de atributo para conexion de la base de datos
 
     @FXML
     BorderPane home;
@@ -34,14 +43,24 @@ public class LoginController implements Initializable{//, ControlledScreen {
     TextField txtUser;
     @FXML
     PasswordField txtPass;
-   
+
+    private void conectar() {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Contabilidad", "sistemascontables", "SistemasContables");
+
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         btnLoginAction();
     }    
     
     @FXML
-    public void btnLoginAction()  {
+    public void btnLoginAction() {
         btnLogin.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -49,20 +68,18 @@ public class LoginController implements Initializable{//, ControlledScreen {
                     Stage stage = new Stage();
                     stage.setTitle("Sistema Contable");
                     home = FXMLLoader.load(getClass().getResource("vistas/HOME.fxml"));
-                    Scene scene = new Scene (home);
+                    Scene scene = new Scene(home);
                     stage.setScene(scene);
                     stage.show();
-                    
-                } 
-                catch (IOException ex) {
-                    Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+                   ((Node) (event.getSource())).getScene().getWindow().hide();
+
+                } catch (IOException ex) {
+                     Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
                 }
                 ((Node)(event.getSource())).getScene().getWindow().hide();
             }
         });
-        
-        
-     
+
     }
-    
+
 }
